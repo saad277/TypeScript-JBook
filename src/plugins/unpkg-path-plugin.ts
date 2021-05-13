8321998c726e1f4f3d4092aba4e960a8f83d488f
saad277 <saadsheikh27@gmail.com>
2021-05-13 13:11:44 +0500

    index db cache

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localforage from "localforage";
+
+const fileCache = localforage.createInstance({
+  name: "filecache",
+});
 
 export const unpkgPathPlugin = () => {
   return {
@@ -41,13 +46,32 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        //check if file is present
+
+        // if in cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        //if present return
+
         const { data, request } = await axios.get(args.path);
 
-        return {
+        //store in cache
+
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
